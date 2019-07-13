1a1c2ebafad6537025eadb0ff44f6f32e9841b51
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-13 23:55:24 +0700

    Add files via upload
    add find affection bonus stat function

---
+++ commands/find/find_aff.js
@@ -0,0 +1,41 @@
+const commando = require('discord.js-commando');
+const puppeteer = require('puppeteer');
+
+class FindAff extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'aff',
+            group: 'find',
+            memberName: 'aff',
+            description: 'find aff stats of an unit'
+        });
+    }
+
+    async run(message, input) {
+        var unit = toTitleCase(input);
+        var link = "https://aigis.fandom.com/wiki/" + unit;
+        
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.goto(link);
+        
+        const textContent = await page.evaluate(() => {
+            if ( $( '.listtable.bgwhite' ).length ) {
+                var temp = $('.listtable.bgwhite tr')[2].getElementsByTagName("td")[11].innerText;
+                return temp;
+            }
+            else return "not found";
+        });
+
+        message.channel.send(textContent);
+        await browser.close();
+    }
+}
+// capitalize
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+module.exports = FindAff;
