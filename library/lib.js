f420f3ffe3b78b6752892c6f0b30b79342e093ac
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-17 03:18:58 +0700

    Add files via upload

---
+++ library/lib.js
@@ -0,0 +1,19 @@
+var name = {
+    'Snekloli' : 'Orochihime',
+    'Shampoo' : 'Jin-Guang Sheng-Pu',
+    'Gelbro' : 'Gellius',
+    'Shoka' : 'Shokatsuryou',
+    'White Fluff' : 'Kyuubi',
+    'Gold Fluff' : 'Kayou',
+    'Chocorino' : 'Rino (Valentine\'s)',
+    'Chainloli' : 'Salus',
+    'Hammerloli' : 'Teeny',
+    'Catloli' : 'Minette',
+    'Danchou' : 'Dark Knight',
+    'Dark' : 'D\'Arc',
+    'Cucumber' : 'Kyuubi',
+    'Ate' : 'Kibahime',
+    'Kure' : 'Kanon'
+}
+
+exports.name = name;
