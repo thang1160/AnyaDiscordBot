19addb3bb7b3767fbed4ee54dd8e2a7b698b9189
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-17 03:18:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
 
-bot.registry.registerGroup('find', 'Random');
+bot.registry.registerGroup('find', 'Find');
+bot.registry.registerGroup('timer', 'Timer');
+bot.registry.registerGroup('compare', 'Compare');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
