4d43f8d1302a8ad1bfd69de216ff0faa47f8a4df
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-17 15:51:47 +0700

    add find which date can get input orb

--- commands/daily/daily_quest.js
+++ commands/daily/daily_quest.js
@@ -12,23 +12,49 @@ class Maintenance extends commando.Command {
 
     async run(message, input) {
         message.channel.send("Note: Sunday is 0, Monday is 1, and so on till 6\n");
-        var textArray = [
-            'Demon crystal\nOrbs: Alchemist, Curse User, Puppeteer, Thief, Bowrider',
-            'Armor\nOrbs: Archer, Dark Fighter, Healer, Rearguard Tactician, Gunner',
-            'Spirit\nOrbs: Valkyrie, Pegasus Rider, Monk, Bandit, Feng Shui User',
-            'Demon crystal\nOrbs: Soldier, Rogue, Pirate, Shaman, Bishop',
-            'Affection items\nOrbs: Heavy Armor, Samurai, Magic Fencer, Vampire Hunter, Sailor',
-            'Armor\nOrbs: Witch, Warlock, Vanguard Tactician, Ninja, Angel',
-            'Spirit\nOrbs: Mage Armor, Ranger, Dancer, Dragon Rider, Priest Warrior'
-        ];
+        var textArray = {
+            0 : 'Demon crystal\nOrbs: Alchemist, Curse User, Puppeteer, Thief, Bowrider',
+            1 : 'Armor\nOrbs: Archer, Dark Fighter, Healer, Rearguard Tactician, Gunner',
+            2 : 'Spirit\nOrbs: Valkyrie, Pegasus Rider, Monk, Bandit, Feng Shui User',
+            3 : 'Demon crystal\nOrbs: Soldier, Rogue, Pirate, Shaman, Bishop',
+            4 : 'Affection items\nOrbs: Heavy Armor, Samurai, Magic Fencer, Vampire Hunter, Sailor',
+            5 : 'Armor\nOrbs: Witch, Warlock, Vanguard Tactician, Ninja, Angel',
+            6 : 'Spirit\nOrbs: Mage Armor, Ranger, Dancer, Dragon Rider, Priest Warrior'
+        }
+        var number = {
+            0 : 'Sunday',
+            1 : 'Monday',
+            2 : 'Tuesday',
+            3 : 'Wednesday',
+            4 : 'Thursday',
+            5 : 'Friday',
+            6 : 'Saturday'
+        }
         var date = parseInt(input);
+        var check = false;
         if(date != NaN)
         {
             if(date >= 0 && date <= 6)
-                message.channel.send(textArray[date]);
+                {
+                    message.channel.send(textArray[date]);
+                    check = true;
+                }
+        }
+        else if(input.length >= 3)
+        {
+            for (let i = 0; i < textArray.length; i++) {
+                const element = textArray[i];
+                if(element.includes(input))
+                {
+                    message.channel.send(number[i] + "\n");
+                    check = true;
+                }
+            }
+            if(check = false)
+                message.channel.send("Input wrong, please try again");
         }
         else
-            message.channel.send("Input wrong, please try again");
+            message.channel.send("Input too short, please try again");
     }
 }
 
