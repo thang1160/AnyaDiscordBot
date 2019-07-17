bc286e978c9d7a80cac699376432d32597f73b91
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-17 13:58:14 +0700

    Add files via upload

---
+++ commands/random/nou.js
@@ -0,0 +1,23 @@
+const commando = require('discord.js-commando');
+
+class Maintenance extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'nou',
+            group: 'random',
+            memberName: 'nou',
+            description: 'no u'
+        });
+    }
+
+    async run(message, input) {
+        var textArray = [
+            'https://media.discordapp.net/attachments/176380280293163008/558171167861506074/Nou.gif',
+            'https://tenor.com/view/two-spiderman-pointing-meme-gif-11796793'
+        ];
+        var randomNumber = Math.floor(Math.random()*textArray.length);
+        message.channel.send(textArray[randomNumber]);
+    }
+}
+
+module.exports = Maintenance;
