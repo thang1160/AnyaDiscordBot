50f0d2a831ec8665832990631f8f1f714a6bf4f9
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-17 13:11:59 +0700

    Update maintenance.js

--- commands/timer/maintenance.js
+++ commands/timer/maintenance.js
@@ -21,24 +21,22 @@ class Maintenance extends commando.Command {
         lastday = new Date(lastday.setHours(1));
         lastday = new Date(lastday.setMinutes(0));
         lastday = new Date(lastday.setSeconds(0));
-        const thurday = lastday;
+        const thursday = lastday;
 
-        var diffTime = Math.abs(thurday.getTime() - today.getTime());
+        var diffTime = Math.abs(thursday.getTime() - today.getTime());
         var totalDiffMinutes = Math.ceil(diffTime / (1000 * 60));
         var diffDays = parseInt(totalDiffMinutes/(60*24));
         var diffHours = parseInt((totalDiffMinutes - diffDays*24*60)/60);
         var diffMinutes = (totalDiffMinutes - diffDays*24)%60;
-        message.channel.send(thurday);
         message.channel.send("Maintenance start in: \nDay(s): " + diffDays + "   Hour(s): " + diffHours + "   Minute(s): " + diffMinutes);
 
-        lastday = thurday;
-        lastday.setHours(thurday.getHours() + 5);
+        lastday = thursday;
+        lastday.setHours(thursday.getHours() + 5);
         diffTime = Math.abs(lastday.getTime() - today.getTime());
         totalDiffMinutes = Math.ceil(diffTime / (1000 * 60));
         diffDays = parseInt(totalDiffMinutes/(60*24));
         diffHours = parseInt((totalDiffMinutes - diffDays*24*60)/60);
         diffMinutes = (totalDiffMinutes - diffDays*24)%60;
-        message.channel.send(lastday);
         message.channel.send("Maintenance finish in: \nDay(s): " + diffDays + "   Hour(s): " + diffHours + "   Minute(s): " + diffMinutes);
     }
 }
