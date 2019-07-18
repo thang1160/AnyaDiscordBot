7e138a21f0768a4a217ea2fb6b73ccfabc1b302e
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-18 20:22:46 +0700

    Update find_aff.js

--- commands/find/find_aff.js
+++ commands/find/find_aff.js
@@ -2,6 +2,7 @@ const commando = require('discord.js-commando');
 var request = require('request');
 var cheerio = require('cheerio');
 var name = require('../../library/lib.js').name;
+var functions = require('../../functions.js');
 
 class FindAff extends commando.Command {
     constructor(client) {
@@ -14,55 +15,44 @@ class FindAff extends commando.Command {
     }
 
     async run(message, input) {
-        var unit = toTitleCase(input);
+        var unit = functions.toTitleCase(input);
         var gold = unit.startsWith("Gold");
         var platinum = unit.startsWith("Plat ");
         var black = unit.startsWith("Black");
         var link;
-        if(gold == true)
-        {
+        if (gold == true) {
             link = "https://aigis.fandom.com/wiki/150_Affection_Bonus_-_Gold";
             unit = unit.substring(5, unit.length);
         }
-        if(platinum == true)
-        {
+        if (platinum == true) {
             link = "https://aigis.fandom.com/wiki/150_Affection_Bonus_-_Platinum";
             unit = unit.substring(5, unit.length);
         }
-        if(black == true)
-        {
+        if (black == true) {
             link = "https://aigis.fandom.com/wiki/150_Affection_Bonus_-_Black";
             unit = unit.substring(6, unit.length);
         }
-        if(name[unit]) unit = name[unit];
-        
-        if(link)
-        {
+        if (name[unit]) unit = name[unit];
+
+        if (link) {
             request(link, function (err, resp, html) {
                 if (!err) {
-                        const $ = cheerio.load(html);
-                        var jquery = "tr:contains('" + unit + "')";
-                        var output = $(jquery).html();
-                        if(output)
-                        {
-                            output = output.replace(/<[^>]*>/g,"\n");
-                            output = output.replace(/\n+/g,"\n");
-                            message.channel.send(output);
-                        }
-                        else
-                            message.channel.send(input + " not found");
+                    const $ = cheerio.load(html);
+                    var jquery = "tr:contains('" + unit + "')";
+                    var output = $(jquery).html();
+                    if (output) {
+                        output = output.replace(/<[^>]*>/g, "\n");
+                        output = output.replace(/\n+ /g, "\n");
+                        message.channel.send(output.trim());
+                    }
+                    else
+                        message.channel.send(input + " not found");
                 }
             });
         }
         else
             message.channel.send("you need to add rarity before unit's name");
     }
-}
-// capitalize
-function toTitleCase(str) {
-    return str.replace(/\w+('s)?/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
 }
 
 module.exports = FindAff;
