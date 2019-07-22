ad19c29229f0bb02104afcba2a36111bf59fb0e0
thang1160 <35557579+thang1160@users.noreply.github.com>
2019-07-23 01:20:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ bot.registry.registerGroup('compare', 'Compare');
 bot.registry.registerGroup('random', 'Random');
 bot.registry.registerGroup('daily', 'Daily');
 bot.registry.registerGroup('fame', 'Fame');
+bot.registry.registerGroup('list', 'List');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
