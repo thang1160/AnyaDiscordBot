55849449a08c1b490c0970c27f98353151a04317
thang1160 <thang1160@users.noreply.github.com>
2019-08-02 20:40:49 +0700

    Update maintenance.js

--- commands/timer/maintenance.js
+++ commands/timer/maintenance.js
@@ -15,25 +15,25 @@ class Maintenance extends commando.Command {
         japanTime = new Date(japanTime);
         message.channel.send('JAP time: ' + japanTime.toLocaleString());
 
-        var curr = new Date;
-        var thurs = curr.getDate() - curr.getDay() + 4;
-        var thursday = new Date(curr.setDate(thurs)).toLocaleString("en-US", { timeZone: "Japan" });
-        var maintenance = new Date(thursday);
-        maintenance.setHours(11);
-        maintenance.setMinutes(0);
-        maintenance.setSeconds(0);
-        if (japanTime > maintenance === false)
-            message.channel.send('Maintenance start: ' + maintenance.toLocaleString());
-        else {
-            var curr = new Date;
-            var thurs = curr.getDate() - curr.getDay() + 4 + 7;
-            var thursday = new Date(curr.setDate(thurs)).toLocaleString("en-US", { timeZone: "Japan" });
-            var maintenance = new Date(thursday);
-            maintenance.setHours(11);
-            maintenance.setMinutes(0);
-            maintenance.setSeconds(0);
-            message.channel.send('Maintenance start: ' + maintenance.toLocaleString());
-        }
+        // var curr = new Date;
+        // var thurs = curr.getDate() - curr.getDay() + 4;
+        // var thursday = new Date(curr.setDate(thurs)).toLocaleString("en-US", { timeZone: "Japan" });
+        // var maintenance = new Date(thursday);
+        // maintenance.setHours(11);
+        // maintenance.setMinutes(0);
+        // maintenance.setSeconds(0);
+        // if (japanTime > maintenance === false)
+        //     message.channel.send('Maintenance start: ' + maintenance.toLocaleString());
+        // else {
+        //     var curr = new Date;
+        //     var thurs = curr.getDate() - curr.getDay() + 4 + 7;
+        //     var thursday = new Date(curr.setDate(thurs)).toLocaleString("en-US", { timeZone: "Japan" });
+        //     var maintenance = new Date(thursday);
+        //     maintenance.setHours(11);
+        //     maintenance.setMinutes(0);
+        //     maintenance.setSeconds(0);
+        //     message.channel.send('Maintenance start: ' + maintenance.toLocaleString());
+        // }
         // var milidif = maintenance-japanTime;
         // var daydif = Math.floor(milidif/1000/3600/24);
         // var hourdif = Math.floor((milidif - daydif*24*3600*1000)/1000/3600);
